91f67256afb911008e79b44a585afe7d57516daa
ioott <vioott@gmail.com>
2022-10-27 20:57:37 -0300

    build: atualizado endpoint no frontend para deploy no Railway

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tfc-back-end-production.up.railway.app/' || '3001',
+  baseURL: 'https://tfc-back-end-production.up.railway.app/',
 });
 
 export const setToken = (token) => {
